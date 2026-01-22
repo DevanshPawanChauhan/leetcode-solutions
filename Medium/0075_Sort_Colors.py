39940b073a1f1aabe99391feb4c59a851c8a9131
DevanshPawanChauhan <chauhandevansh7203@gmail.com>
2026-01-22 14:31:04 +0530

    Add solution for LeetCode 75 - Sort Colors (Dutch National Flag).

---
+++ Medium/0075_Sort_Colors.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        n=len(nums)
+        left=0
+        right=n-1
+        mid=0
+        while mid<=right:
+            if nums[mid]==0:
+                nums[mid],nums[left]=nums[left],nums[mid]
+                mid+=1
+                left+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[right]=nums[right],nums[mid]
+                right-=1
