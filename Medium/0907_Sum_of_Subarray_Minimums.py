122794cb14d684c919debbe36243009517a4023c
DevanshPawanChauhan <chauhandevansh7203@gmail.com>
2026-02-12 18:04:54 +0530

    feat(stack): implement 0907_Sum_of_Subarray_Minimums using contribution technique

---
+++ Medium/0907_Sum_of_Subarray_Minimums.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        MOD=(10**9)+7
+        stack=[]
+        n=len(arr)
+        left=[0]*n
+        right=[0]*n
+        for i in range(n):
+            while stack and arr[stack[-1]]>arr[i]:
+                stack.pop()
+            if not stack:
+                left[i]=i+1
+            else:
+                left[i]=i-stack[-1]
+            stack.append(i)
+        stack=[]
+        for i in range(n-1,-1,-1):
+            while stack and arr[stack[-1]]>=arr[i]:
+                stack.pop()
+            if not stack:
+                right[i]=n-i
+            else:
+                right[i]=stack[-1]-i
+            stack.append(i)
+        ans = 0
+        for i in range(n):
+            ans = (ans + arr[i] * left[i] * right[i]) % MOD
+
+        return ans
